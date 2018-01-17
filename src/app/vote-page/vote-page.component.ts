8d59266ba998950b8595df79755755c1ad4e9c15
Ravi Prakash <ravi.prakash@kronos.com>
2018-01-17 10:16:52 +0530

    Basic App structure.

---
+++ src/app/vote-page/vote-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-vote-page',
+  templateUrl: './vote-page.component.html',
+  styleUrls: ['./vote-page.component.css']
+})
+export class VotePageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
